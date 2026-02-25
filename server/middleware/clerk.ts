c8af83ee0dc71c2e2ec95964b541d8b835b422bd
John Smith <johnsmith@esi.info>
2026-02-25 13:06:57 +0000

    removed server side clerk stuff

--- server/middleware/clerk.ts
+++
@@ -1,9 +0,0 @@
-import { clerkMiddleware } from '@clerk/nuxt/server'
-import { getRequestURL } from 'h3'
-
-export default defineEventHandler((event) => {
-  return clerkMiddleware({
-    domain: getRequestURL(event).host,
-    signInUrl: `${process.env.NUXT_PUBLIC_MAIN_SITE_URL ?? 'http://localhost:3000'}/login`,
-  })(event)
-})
